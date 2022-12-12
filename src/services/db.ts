18cc9e7b19bf3c37497367af004867e6fbcbb820
Vlad Faust <hey@vladfaust.com>
2022-12-12 16:43:12 +0400

    feat: debug db path on open

--- src/services/db.ts
+++ src/services/db.ts
@@ -2,7 +2,10 @@ import config from "@/config.js";
 import Database from "better-sqlite3";
 
 export function open(trace: boolean = false) {
-  const db = new Database(config.db.url.host + config.db.url.pathname, {
+  const path = config.db.url.host + config.db.url.pathname;
+  console.debug("Opening database connection", path);
+
+  const db = new Database(path, {
     fileMustExist: true,
     verbose: trace ? console.log : undefined,
   });
