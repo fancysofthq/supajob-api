eae3facc3f46c03ff33f2d5c482eab3adf99dac8
Vlad Faust <hey@vladfaust.com>
2022-12-16 17:38:28 +0400

    fix(eth): give provider more time

--- src/services/eth.ts
+++ src/services/eth.ts
@@ -4,7 +4,7 @@ import { timeout } from "@/utils.js";
 
 console.log("Connecting to JSON-RPC provider at", config.eth.rpcUrl);
 const provider = new ethers.providers.JsonRpcProvider(config.eth.rpcUrl);
-await timeout(1000, provider.ready, "Provider not ready");
+await timeout(5000, provider.ready, "Provider not ready");
 
 const blockNumber = await provider.getBlockNumber();
 console.log("Current block number:", blockNumber);
