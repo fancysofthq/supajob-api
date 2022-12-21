428eff01a02c1c4537062b404768f58b716193cc
Vlad Faust <hey@vladfaust.com>
2022-12-22 02:48:24 +0400

    feat: wss provider

--- src/services/eth.ts
+++ src/services/eth.ts
@@ -2,6 +2,15 @@ import config from "../config.js";
 import { ethers } from "ethers";
 import { timeout } from "@/utils.js";
 
+let provider;
+
 console.log("Connecting to JSON-RPC provider at", config.eth.rpcUrl);
-export const provider = new ethers.providers.JsonRpcProvider(config.eth.rpcUrl);
+if (config.eth.rpcUrl.startsWith("ws")) {
+  provider = new ethers.providers.WebSocketProvider(config.eth.rpcUrl);
+} else {
+  provider = new ethers.providers.JsonRpcProvider(config.eth.rpcUrl);
+}
+
 await timeout(5000, provider.ready, "JSON-RPC provider not ready");
+
+export { provider };
